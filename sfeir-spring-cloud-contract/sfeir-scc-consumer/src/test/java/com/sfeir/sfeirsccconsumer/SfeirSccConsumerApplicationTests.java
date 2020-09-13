5f8f35fbf1f59c9abc1462480d4c7cf759b2d6a9
Tine Kondo <kondotine@gmail.com>
2020-09-13 16:52:18 +0200

    chore(sfeir-spring-cloud-contract): initial commit

---
+++ sfeir-spring-cloud-contract/sfeir-scc-consumer/src/test/java/com/sfeir/sfeirsccconsumer/SfeirSccConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfeir.sfeirsccconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfeirSccConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sfeir-spring-cloud-contract/sfeir-scc-producer/src/test/java/com/sfeir/scc/producer/SfeirSccProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfeir.scc.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfeirSccProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
